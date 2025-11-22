2ac38bdc9d82660f72a29de002e04ee053dfae10
OutfitChecked <serratca@gmail.com>
2025-11-22 14:33:29 -0600

    Actualicé el modelo Usuario

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./db.js";
 
 import usuariosRoutes from "./routes/usuariosRoutes.js";
 import prendasRoutes from "./routes/prendasRoutes.js";
 import estilosRoutes from "./routes/estilosRoutes.js";
 import outfitsRoutes from "./routes/outfitsRoutes.js";
 
+dotenv.config();
+
 const app = express();
+
+connectDB();
 
 app.use(cors());
 app.use(express.json());
