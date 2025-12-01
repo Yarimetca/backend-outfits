7fdc01146fabee84c5ae2dfda957381f427e989d
OutfitChecked <serratca@gmail.com>
2025-12-01 01:33:08 -0600

    Limpieza: eliminado src/index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,9 +6,11 @@ import clothesRoutes from "./clothesRoutes.js";
 import outfitRoutes from "./outfitRoutes.js";
 
 const router = Router();
+
 router.use("/auth", authRoutes);
 router.use("/users", userRoutes);
 router.use("/categories", categoryRoutes);
 router.use("/clothes", clothesRoutes);
 router.use("/outfits", outfitRoutes);
+
 export default router;
