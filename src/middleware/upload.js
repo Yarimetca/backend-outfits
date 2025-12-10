4d5111c8b864bf4da6e66dbf0d8280aded27ed2e
OutfitChecked <serratca@gmail.com>
2025-12-10 10:36:14 -0600

    Actualicé el modelo

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -1,39 +1,21 @@
 import multer from "multer";
-import path from "path";
 import fs from "fs";
 
-const uploadsPath = path.join(process.cwd(), "uploads");
+const uploadPath = "/tmp/uploads"; 
 
-if (!fs.existsSync(uploadsPath)) {
-  fs.mkdirSync(uploadsPath);
+if (!fs.existsSync(uploadPath)) {
+  fs.mkdirSync(uploadPath, { recursive: true });
 }
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, uploadsPath);
+  destination: (req, file, cb) => {
+    cb(null, uploadPath);
   },
-  filename: function (req, file, cb) {
-    const uniqueName =
-      Date.now() + "-" + file.originalname.replace(/\s+/g, "_");
-    cb(null, uniqueName);
-  },
-});
-
-function fileFilter(req, file, cb) {
-  const allowedTypes = ["image/jpeg", "image/png", "image/jpg", "image/webp"];
-
-  if (allowedTypes.includes(file.mimetype)) {
-    cb(null, true);
-  } else {
-    cb(new Error("Solo se permiten archivos de imagen."), false);
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname.replace(/\s+/g, "_"));
   }
-}
-
-
-const upload = multer({
-  storage,
-  fileFilter,
-  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB
 });
+
+const upload = multer({ storage });
 
 export default upload;
