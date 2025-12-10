e037453dd3f2350aa53ae6e7dea97b4c8d6b215c
OutfitChecked <serratca@gmail.com>
2025-12-10 05:55:59 -0600

    Fix PORT for Render

--- src/server.js
+++ src/server.js
@@ -1,18 +1,20 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 import routes from "./routes/index.js";
 import prisma from "./prisma/client.js";
 
 dotenv.config();
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 
 app.use("/api", routes);
-
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, async () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
@@ -23,3 +25,4 @@ app.listen(PORT, async () => {
     console.error("Prisma connect error:", err);
   }
 });
+
