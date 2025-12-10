09a3703040beb3d0046ae7f8e5cf6d4a018cf860
OutfitChecked <serratca@gmail.com>
2025-12-10 10:33:28 -0600

    Actualicé el modelo

--- src/controllers/clothesController.js
+++ src/controllers/clothesController.js
@@ -130,3 +130,4 @@ export const deleteClothes = async (req, res) => {
     res.status(500).json({ error: "Error al eliminar prenda" });
   }
 };
+const imagePath = req.file ? `/tmp/uploads/${req.file.filename}` : null;
